f98a82e3d9b4fa3c532d3fa1ba73888b6107004c
VeroIO <ITITIU14056@student.hcmiu.edu.vn>
2018-04-25 23:09:37 +0700

    Still Need Edit Categories

--- controllers/admin/notesmanagement.js
+++ controllers/admin/notesmanagement.js
@@ -2,7 +2,9 @@ var express = require("express");
 var router = express.Router();
 var mCategories = require("../../models/categories");
 const paginate = require("express-paginate");
+
 router.use(paginate.middleware(10, 50));
+
 router.route("").get(isLoggedIn,function(req, res) {
     mCategories.findAndCountAll({limit: parseInt(req.query.limit),offset: req.skip}).then(function(results) {
         var cookies = parseCookies(req);
@@ -12,7 +14,7 @@ router.route("").get(isLoggedIn,function(req, res) {
             req.query.page=0;
         }
         res.render('admin/notesmanagement', {
-            user : req.user,
+            user : req.user,    
             token: cookies.jwt,
             categories: results.rows,
             pageCount,
@@ -20,6 +22,32 @@ router.route("").get(isLoggedIn,function(req, res) {
             pages: paginate.getArrayPages(req)(3, pageCount, parseInt(req.query.page)),
         });
     });
+});
+router.route("").post(isLoggedIn,function(req, res) {
+    type=req.body.type;
+    if(type=="xoacate"){
+        mCategories.destroy({ where: { id: req.body.id } }).then(function(results) {
+            if(results==1){
+                res.send(("Đã Thành Xóa Thành Công ID:" + req.body.id));
+            }else{
+                res.send("Thao Tác Không Thành Công");
+            }
+        });
+    }else if(type=="addcate"){
+        var newCategory = {
+          type: req.body.cate_type,
+          name: req.body.cate_name
+        };
+        mCategories.create(newCategory).then(function(task) {
+            res.send("Đã Thêm Thành Công : " + req.body.cate_name+"Vào Categories");
+        }).catch(function (err) {
+            res.send("Thao Tác Không Thành Công");
+        })
+    }else if(type=="editcate"){
+
+    }else{
+        res.send("Error")
+    }
 });
 function parseCookies (request) {
     var list = {},
